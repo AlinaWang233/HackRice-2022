1437967b9f80ef7500f23cac2bb9e8b7103e51d2
Anita-Zhou <1011433458@qq.com>
2022-09-24 00:49:45 -0500

    backend

--- backend/member.py
+++ backend/member.py
@@ -4,7 +4,7 @@
 The class for rescuers and survivors
 '''
 class HSMember (object):
-    def __init__(self, name, tel, email):
+    def __init__(self, name, tel, address):
         self.name = name
         self.tel = tel
         self.email = email
